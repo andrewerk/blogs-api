d1a228be307c8bf9689b1394d569e9f85453eed7
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-06-22 18:32:48 -0300

    blogPost model implemented

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -8,7 +8,11 @@ const User = (sequelize, DataTypes) => {
   }, {
     timestamps: false
   });
-
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'blogposts'
+    })
+  }
   return User;
 };
 
