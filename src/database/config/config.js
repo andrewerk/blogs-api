48fa7899e3836cd3baf0c46c207f6fdef5730679
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-06-30 11:41:34 -0300

    Database Postgres bugs

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,29 +1,20 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'production';
-
-const suffix = {
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
-
 const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
+  database: process.env.MYSQL_DB_NAME,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '1234',
   dialect: process.env.DB_DIALECT,
-  dialectOptions: {
-    timezone: 'Z',
-  },
   logging: process.env.DEBUG !== 'false',
 };
 
 module.exports = {
   development: {
+    ...options,
+  },
+  production: {
     ...options,
   },
   test: {
