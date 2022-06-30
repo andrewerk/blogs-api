632ebb815356016dadf6d497d536212785df83df
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-06-30 14:31:46 -0300

    Setting project to github

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 
 const options = {
   host: process.env.HOSTNAME || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: process.env.MYSQL_DB_NAME,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
+  database: process.env.POSTGRES_DB_NAME,
+  username: process.env.POSTGRES_USER || 'root',
+  password: process.env.POSTGRES_PASSWORD || '1234',
   dialect: process.env.DB_DIALECT,
   logging: process.env.DEBUG !== 'false',
 };
