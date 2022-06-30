3015cba0d9d52e7135423463b9ef5528fcc650d6
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-06-30 12:19:40 -0300

    DB config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+  host: process.env.HOSTNAME || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
   database: process.env.MYSQL_DB_NAME,
   username: process.env.MYSQL_USER || 'root',
